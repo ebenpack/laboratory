0fd18788f2ffeb22c97bb57b7c38e28ee06e94b8
Eben Packwood <ebenpackwood@gmail.com>
2013-11-22 15:11:13 -0500

    Fix PEP8 issues.

--- cribbage/cribbage/event.py
+++ cribbage/cribbage/event.py
@@ -1,3 +1,5 @@
+#TODO: Refactor events. Initialize w/ players?
+
 class Event(object):
     def discard(self):
         pass
@@ -7,6 +9,10 @@ class Event(object):
 
     def peg(self):
         pass
+
+    def muggins(self):
+        pass
+
 
 class OfflineEvent(Event):
     def discard(self, player):
@@ -21,21 +27,33 @@ class OfflineEvent(Event):
         print("Your cards: {}".format(player.hand.unplayed))
         print("Select a card (by index) to play.")
         played = int(input())
-        card = player.hand.play( played )
+        card = player.hand.play(played)
         return card
 
-    def peg(self):
-        pass
+    def peg(self, player):
+        print("Your cards: {}".format(player.hand))
+        print("How many points in your hand?")
+        score = int(input())
+        return score
+
+    def muggins(self):
+        print("How many muggins?")
+        points = int(input())
+        return points
+
 
 class MockEvent(Event):
     def discard(self, player):
-        discarded = [0,1]
+        discarded = [0, 1]
         return discarded
 
     def play(self, player):
         played = 0
-        card = player.hand.play( played )
+        card = player.hand.play(played)
         return card
 
-    def peg(self):
+    def peg(self, player):
+        return 0
+
+    def muggins(self):
         pass
