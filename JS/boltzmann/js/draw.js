3142afc0d8fb33527ed76777f0df3acb4853251c
Eben Packwood <ebenpackwood@gmail.com>
2014-01-18 11:15:48 -0500

    Update draw color handling

--- JS/boltzmann/js/draw.js
+++ JS/boltzmann/js/draw.js
@@ -3,7 +3,7 @@ function draw_lattice() {
     var lattice_width = lattice.length;
     var lattice_height = lattice[0].length;
     var px_per_node = Math.floor(canvas.width / lattice_width);
-    if (canvas.getContext){
+    if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
         var vectorctx = vectorcanvas.getContext('2d');
 
@@ -20,50 +20,36 @@ function draw_lattice() {
                 if (lattice[x][y].barrier) {
                     draw_square(x, y, 0, 0, 0, 0);
                 } else {
-                    var r = 0;
-                    var g = 0;
-                    var b = 0;
-                    var a = 255;
+                    var color = {'r': 0, 'g': 0, 'b': 0, 'a': 0};
                     var ux = lattice[x][y].ux;
                     var uy = lattice[x][y].uy;
                     if (draw_mode === 0) {
                         // Speed
-                        g = Math.sqrt(Math.pow(ux, 2) + Math.pow(uy, 2)) * 2000;
+                        var speed = Math.sqrt(Math.pow(ux, 2) + Math.pow(uy, 2));
+                        color = get_color(speed, 0, 0.4);
+                        color = {'r': 0, 'g': (speed*3000), 'b': 0, 'a': 255};
+                        if (color.g > 255) {color.g = 255;}
+                        if (color.g < 0) {color.g = 0;}
                     } else if (draw_mode == 1) {
                         // X velocity
-                        var xvel = ux * 3000;
-                        if (xvel < 0) {
-                            g = Math.abs(xvel);
-                        } else {
-                            r = Math.abs(xvel);
-                        }
+                        var xvel = ux;
+                        color = get_color(xvel, -0.1, 0.1);
                     } else if (draw_mode == 2) {
                         // Y Velocity
-                        var yvel = uy * 3000;
-                        if (yvel < 0) {
-                            g = Math.abs(yvel);
-                        } else {
-                            r = Math.abs(yvel);
-                        }
+                        var yvel = uy;
+                        color = get_color(yvel, -0.1, 0.1);
                     } else if (draw_mode == 3) {
                         // Density
-                        g = 255 - (255 / Math.abs(lattice[x][y].density));
-                        g = g * 10;
+                        var dens = lattice[x][y].density;
+                        color = {'r': 0, 'g': ((255 - (255 / Math.abs(dens)))*10), 'b': 0, 'a': 255};
+                        if (color.g > 255) {color.g = 255;}
+                        if (color.g < 0) {color.g = 0;}
                     } else if (draw_mode == 4) {
                         // Curl
                         var curl = lattice[x][y].curl;
-                        if (curl > 0) {
-                            g = Math.abs(curl) * 2000;
-                        } else {
-                            r = Math.abs(curl) * 2000;
-                        }
+                        color = get_color(curl, -0.2, 0.2);
                     }
-                    //var ang = angle(0,0, lattice[x][y].ux, lattice[x][y].uy);
-                    if (g > 255) {g = 255;}
-                    if (g < 0) {g = 0;}
-                    if (r > 255) {r = 255;}
-                    if (r < 0) {r = 0;}
-                    draw_square(x, y, r, g, b, a);
+                    draw_square(x, y, color);
                     if (draw_flow_vectors && x % 10 === 0 && y % 10 ===0) {
                         // Draw flow vectors every tenth node.
                         // TODO: Some flow vectors appear to be going in the wrong direction.
@@ -75,21 +61,20 @@ function draw_lattice() {
         }
         ctx.putImageData(image, 0, 0);
     }
-    function draw_square(x, y, r, g, b, a) {
+    function draw_square(x, y, color) {
         // Draw a square region on the canvas image corresponding to a
         // lattice node at (x,y).
         for (var ypx = y * px_per_node; ypx < (y+1) * px_per_node; ypx++) {
             for (var xpx = x * px_per_node; xpx < (x + 1) * px_per_node; xpx++) {
                 var index = (xpx + ypx * image.width) * 4;
-                image.data[index+0] = r;
-                image.data[index+1] = g;
-                image.data[index+2] = b;
-                image.data[index+3] = a;
+                image.data[index+0] = color.r;
+                image.data[index+1] = color.g;
+                image.data[index+2] = color.b;
+                image.data[index+3] = color.a;
             }
         }
     }
     function draw_flow_vector(x,y,ux,uy) {
-        // Translate y
         var scale = 100;
         vectorctx.beginPath();
         vectorctx.moveTo(x * px_per_node, y * px_per_node);
@@ -99,18 +84,27 @@ function draw_lattice() {
         vectorctx.arc(x * px_per_node, y * px_per_node, 1, 0, 2 * Math.PI, false);
         vectorctx.fill();
     }
-}
-
-function angle(x1, y1, x2, y2) {
-    // Return angle in radians, with 'North' being 0,
-    // increasing clockwise to 3pi/2 at 'East', pi at 'South', etc.
-    var dx = x1 - x2;
-    var dy = y1 - y2;
-    var theta = Math.atan2(-dx,dy);
-    // atan2 returns results in the range -pi...pi. Convert results 
-    // to the range 0...2pi
-    if (theta < 0) {
-        theta += (2 * Math.PI);
+    function get_color(val, min, max) {
+        // Returns a color for a given value in a range between min and max.
+        // Min and max were experimentally derived for speed, density, etc.
+        var mid = (min + max) / 2;
+        var range = Math.abs(max-mid);
+        var color = {'r': 0, 'g': 0, 'b': 0, 'a': 0};
+        if (val > max) {
+            val = max;
+        }
+        if (val < min) {
+            val = min;
+        }
+        if (val >= mid) {
+            color.r = 185;
+            color.g = 128;
+            color.a = Math.abs(val) * (1/range) * 255;
+        } else {
+            color.r = 0;
+            color.g = 255;
+            color.a = Math.abs(val) * (1/range) * 255;
+        }
+        return color;
     }
-    return theta;
 }
