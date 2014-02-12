eadf6f8281b4dbb0494035a6f52558d15da7e9b0
ebenpack <ebenpackwood@gmail.com>
2014-02-11 19:03:30 -0500

    Start fresh, remove all nonessentials; ~1900 bytes

--- JS/js1k/globals.js
+++ JS/js1k/globals.js
@@ -1,4 +1,5 @@
-//These variables are provided by the contest.
+// These variables are provided by the contest, but for testing purposes
+// I will define them here.
 a = document.getElementById("js1k");
 b = document.body;
 c = a.getContext("2d");
