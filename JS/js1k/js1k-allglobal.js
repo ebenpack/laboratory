796c3dd618be863087a53557fd9050178eccc0a2
ebenpack <ebenpackwood@gmail.com>
2014-02-13 14:08:35 -0500

    I'm the best man. I did it.; 1023 bytes

--- JS/js1k/js1k-allglobal.js
+++ JS/js1k/js1k-allglobal.js
@@ -6,15 +6,17 @@
 // HEIGHT/WIDTH=600
 // SET FOO='slice' (2bytes)
 // REMOVE LEADING ZEROS (E.G. 0.1)(2bytes)
-// PUT ALL VARIABLES IN GLOBAL SCOPE (possibly 28bytes)
+// PUT ALL VARIABLES IN GLOBAL SCOPE (28bytes)
+// LOOP CANVAS VARIABLE AND RENAME METHODS/PROPERTIES (width, height, style, onmousemove, style) (maybe ~10bytes??)
+// CHANGE setInterval TO while LOOP??? (PROBABLY WON't WORK)
 // 
 // #######################
 // These are givens for the contest, but it helps to have
 // them here so Google closure doesn't use the names
-// a = document.getElementById("js1k");
-// b = document.body;
-// c = a.getContext("2d");
-// d = 999;
+a = document.getElementById("js1k");
+b = document.body;
+c = a.getContext("2d");
+d = 999;
 X="slice";Y=600;Z=99;
 (function(){  // remove IIFE: ~16bytes
 with(Math)S=sqrt,P=pow,F=floor;
@@ -22,7 +24,7 @@ with(Math)S=sqrt,P=pow,F=floor;
 var lattice_dim = 99; // lattice dimensions. 99 saves me 1 byte vs 100. I'm seriously that desperate 
 var lattice_sq = lattice_dim*lattice_dim; // total # of nodes
 var lattice=[]; // lattice
-var x, x_pos, y_pos, d; // loop variables
+var x, x_pos, y_pos; // loop variables
 var eq = []; // Instead of equilibrium() returning an array, we'll just use this one over and over again and hope we don't forget to initialize it before every use
 var init=1; // This is only used once. Is there another variable that could be used instead?
 var WIDTH = 'width';
@@ -31,54 +33,53 @@ var weight;
 // to save some space (although lookup is more complicated, so
 // I'm not 100% sure if I actually gained anything). It might be
 // possible to halve this string by negating the output for certain
-// numbers (e.g. [1-> (1,0), 3->(-1,0)]; [5->(1,-1), 7->(-1,1)]), but 
+// numbers (e.g. 1-> (1,0), 3->(-1,0); 5->(1,-1), 7->(-1,1)), but 
 // the logic to achieve this might actually take longer than the savings
 // it would gain. I may consider this later if I'm very desperate for bytes.
 var ND = " 0 0 1 0 0-1-1 0 0 1 1-1-1-1-1 1 1 1";
 var px_per_node = 6; // Pixels per node
+a[WIDTH] = a.height = 600;
 function equilibrium(ux, uy, rho) {
     // equilibrium
     eq = [];
-    for (D = 0; D < 9; D++) {
+    for (var d = 0; d < 9; d++) {
         // Calculate equilibrium value
-        var idx = D*4;
-        var eu = (ND.slice(idx,idx+2) * ux) + (ND.slice(idx+2,idx+4) * uy);
+        var eu = (ND.slice(d*4,d*4+2) * ux) + (ND.slice(d*4+2,d*4+4) * uy);
         // Find the node weight. I think this is more succinct than keeping 
         // an array of these values
-        if (D) {
-            weight = (D<5)?1/9:1/36;
+        if (d) {
+            weight = (d<5)?1/9:1/36;
         } else {
             weight = 4/9;
         }
         // Equilibrium equation
-        eq[D] = weight * rho * (1 + 3*eu + 4.5*(eu*eu) - 1.5*((ux * ux) + (uy * uy)));
+        eq[d] = weight * rho * (1 + 3*eu + 4.5*(eu*eu) - 1.5*((ux * ux) + (uy * uy)));
         // TODO: Try returning eq. `return` takes up a few bytes, but I'm
         // also doing a lot of gymnastics to copy over eq outside the function
         // so it might be worth it in the end
     }
 }
 function stream(){
-    for (Q = 0; Q < lattice_sq; Q++) {
-        y_pos = F(Q/lattice_dim);
-        x_pos = Q%lattice_dim;
+    for (x = 0; x < lattice_sq; x++) {
+        y_pos = F(x/lattice_dim);
+        x_pos = x%lattice_dim;
         var node = lattice[x_pos][y_pos];
-        for (K = 0; K < 9; K++) {
+        for (var d = 0; d < 9; d++) {
             // Multiply node direction by one to coerce to int
-            H = K*4;
-            var newx = ND.slice(H,H+2)*1 + x_pos;
-            var newy = ND.slice(H+2,H+4)*1 + y_pos;
+            var newx = ND.slice(d*4,d*4+2)*1 + x_pos;
+            var newy = ND.slice(d*4+2,d*4+4)*1 + y_pos;
             // Check if new node is in the lattice
             if (newx >= 0 && newx < lattice_dim &&
                 newy >= 0 && newy < lattice_dim) {
-                lattice[newx][newy].s[K] = node.d[K];
+                lattice[newx][newy].s[d] = node.d[d];
             }
         }
     }
 }
 function collide(){
     // Collide is going to draw and initialize, too, because LOL, why not?
-    I = c.createImageData(600, 600);
-    L = I.data;
+    var image = c.createImageData(600, 600);
+    var id = image.data;
     for (x = 0; x < lattice_sq; x++) {
         y_pos = F(x/lattice_dim);
         x_pos = x%lattice_dim;
@@ -92,45 +93,45 @@ function collide(){
             lattice[x_pos][y_pos].s = eq;
         }
         var node = lattice[x_pos][y_pos];
-        var dist = node.d;
+        var d = node.d;
         // Copy over values from streaming phase.
-        dist = node.s.slice(0);
+        d = node.s.slice(0);
         // Calculate macroscopic density (rho) and velocity (ux, uy)
         // and update values stored in node.
         // TODO: Can this be compacted any more?
-        var d1 = dist[1] + dist[5] + dist[8];
-        var d2 = dist[3] + dist[6] + dist[7];
-        var rho = d1 + d2 + dist[0] + dist[2] + dist[4];
+        var d1 = d[1] + d[5] + d[8];
+        var d2 = d[3] + d[6] + d[7];
+        var rho = d1 + d2 + d[0] + d[2] + d[4];
         node.x = (d1 - d2) / rho;
-        node.y = (dist[4] + dist[7] + dist[8] - dist[2] - dist[5] - dist[6]) / rho;
+        node.y = (d[4] + d[7] + d[8] - d[2] - d[5] - d[6]) / rho;
         // Update values stored in node.
         node.n = rho;
         // Set node equilibrium for each velocity
         equilibrium(node.x, node.y, rho);
-        for (i = 0; i < 9; i++) {
+        for (var i = 0; i < 9; i++) {
             // TODO: TWEAK OMEGA (CURRENTLY 1.7)
-            node.d[i] = dist[i] + (1.7 * (eq[i] - dist[i]));
+            node.d[i] = d[i] + (1.7 * (eq[i] - d[i]));
         }
         // DRAW
-        for (i = 0; i < 36; i++) {
+        for (var xpx = 0; xpx < 36; xpx++) {
             // This loop was way too difficult for me to flatten.
             // I just wasn't getting my head around it. Maybe I
             // should take a break.
-            var index = 4*(i%6+6*x_pos+600*(F(i/6)+6*y_pos));
-            L[index+1] = lattice_sq; // Green. Setting this way above the max 255, just to save 2 bytes
-            L[index+3] = F(S(P(lattice[x_pos][y_pos].x, 2) + P(lattice[x_pos][y_pos].y, 2))*4E3); // Alpha
+            var index = 4*(xpx%6+6*x_pos+600*(F(xpx/6)+6*y_pos));
+            id[index+1] = lattice_sq; // Green. Setting this way above the max 255, just to save 2 bytes
+            id[index+3] = F(S(P(lattice[x_pos][y_pos].x, 2) + P(lattice[x_pos][y_pos].y, 2))*4E3); // Alpha
         }
     }
-    c.putImageData(I, 0, 0);
+    c.putImageData(image, 0, 0);
     init=0;
 }
 
 function mousemove(e){
     // Scale from canvas coordinates to lattice coordinates
-    for (M = -16; M < 17; M++) {
+    for (var x = -16; x < 17; x++) {
         // There's no OOB checks here anymore. It's fine so long as
         // you don't have your console open. Probably.
-        var node = lattice[F(e.layerX / px_per_node + M/5)][F(e.layerY / px_per_node) + M%5];
+        var node = lattice[F(e.layerX / px_per_node + x/5)][F(e.layerY / px_per_node) + x%5];
         // TODO: Tweak strength of "push"
         equilibrium(.1, .1, node.n);
         node.s = eq;
