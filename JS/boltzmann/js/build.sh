703ecbba2bfc32d679500f252a508b791092fdcb
ebenpack <ebenpackwood@gmail.com>
2014-04-04 12:28:39 -0400

    Minify LBM whitespace only for FF performance

--- JS/boltzmann/js/build.sh
+++ JS/boltzmann/js/build.sh
@@ -1,2 +1,2 @@
 rm boltzmann-min.js
-closure --compilation_level ADVANCED_OPTIMIZATIONS --js=init.js --js=draw.js --js=events.js --js=main.js --js_output_file boltzmann-min.js
+closure --compilation_level WHITESPACE_ONLY  --js=init.js --js=draw.js --js=events.js --js=main.js --js_output_file boltzmann-min.js
